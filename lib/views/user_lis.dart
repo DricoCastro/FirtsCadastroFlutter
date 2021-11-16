e0e66822e6219bce20e85a070801fa31d307823b
DricoCastro <adrianosantosdoco@gmail.com>
2021-11-16 16:14:36 -0300

    new parameters provider

--- lib/views/user_lis.dart
+++ lib/views/user_lis.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/data/dummy.users.dart';
 import 'package:myapp/widgets/user_tile.dart';
+import 'package:provider/provider.dart';
+import 'package:myapp/provider/users.dart';
 
 class UserList extends StatelessWidget {
   const UserList({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    final users = {...DUMMY_USERS};
+    final UsersProvider users = Provider.of(context);
     return Scaffold(
       appBar: AppBar(title: const Text('Lista de Usuários'), actions: <Widget>[
         IconButton(onPressed: () {}, icon: const Icon(Icons.person_add_rounded))
       ]),
       body: ListView.builder(
-        itemCount: users.length,
-        itemBuilder: (ctx, i) => UserTile(users.values.elementAt(i)),
+        itemCount: users.count,
+        itemBuilder: (ctx, i) => UserTile(users.byIndex(i)),
       ),
     );
   }
